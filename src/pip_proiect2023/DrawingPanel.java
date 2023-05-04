48d6c69560439a6c77abd4eab42dc6966f63819f
Bogdan Dumitras <126818671+bogdann02@users.noreply.github.com>
2023-05-04 21:31:46 +0300

    Implement on gui1 Jbuttons Closes #19

--- src/pip_proiect2023/DrawingPanel.java
+++ src/pip_proiect2023/DrawingPanel.java
@@ -7,7 +7,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DrawingPanel extends JPanel {
